1d3b6b307579d6bc8bd54120dc63366e31754959
user <user@DESKTOP-Q7SRQOE>
2023-11-13 19:53:08 +0200

    Add project files.

---
+++ new shops/Program.cs
@@ -0,0 +1,44 @@
+﻿string town = Console.ReadLine();
+string product = Console.ReadLine();
+double quantity = double.Parse(Console.ReadLine());
+
+if (town == "sofia")
+{ 
+  if (product == "coffe")
+        Console.WriteLine(0.50 * quantity);
+    else if (product == "water")
+        Console.WriteLine(0.80 * quantity);
+    else if (product == "beer")
+        Console.WriteLine(1.20 * quantity);
+    else if (product == "sweets")
+        Console.WriteLine(1.45 * quantity);
+    else if (product == "peanuts")
+        Console.WriteLine(1.60 * quantity);
+}
+if (town == "plovdiv")
+{
+    if (product == "coffe")
+        Console.WriteLine(0.40 * quantity);
+    else if (product == "water")
+        Console.WriteLine(0.70 * quantity);
+    else if (product == "beer")
+        Console.WriteLine(1.15 * quantity);
+    else if (product == "sweets")
+        Console.WriteLine(1.30 * quantity);
+    else if (product == "peanuts")
+        Console.WriteLine(1.50 * quantity);
+}
+if (town == "varna")
+{
+    if (product == "coffe")
+        Console.WriteLine(0.45 * quantity);
+    else if (product == "water")
+        Console.WriteLine(0.70 * quantity);
+    else if (product == "beer")
+        Console.WriteLine(1.10 * quantity);
+    else if (product == "sweets")
+        Console.WriteLine(1.35 * quantity);
+    else if (product == "peanuts")
+        Console.WriteLine(1.55 * quantity);
+}
+
